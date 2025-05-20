d4791aedf8fdc8ef43c3ad50096cb02109232c98
danielsierraaraujo <daniel.sierra@udla.edu.ec>
2025-05-20 15:48:21 -0500

    segundo commit - Se define una interfaz que obliga a cualquier clase que la implemente a tener métodos para obtener una respuesta del chatbot y guardar esa respuesta en la base de datos.

---
+++ HerreraSierraVargas_ChatBot/Interfaces/IChatBotService.cs
@@ -0,0 +1,8 @@
+﻿namespace HerreraSierraVargas_ChatBot.Interfaces
+{
+    public interface IChatBotService
+    {
+        public Task<string> GetChatbotResponse(string prompt);
+        public Task<Boolean> SaveResponseInDatabase(string ChatbotPrompt, string ChatbotResponse);
+    }
+}
