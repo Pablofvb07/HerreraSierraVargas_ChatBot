f9f9bc8243021c2124119dfac6eaa25326bd74cb
danielsierraaraujo <daniel.sierra@udla.edu.ec>
2025-05-20 15:44:32 -0500

    Primer commit - se crearon los modelos de gemini y openai en base al modelo curl de las apis.

---
+++ HerreraSierraVargas_ChatBot/Models/GeminiModel.cs
@@ -0,0 +1,17 @@
+﻿namespace HerreraSierraVargas_ChatBot.Models
+{
+    public class GeminiPart
+    {
+        public string text { get; set; }
+    }
+
+    public class GeminiContent
+    {
+        public List<GeminiPart> parts { get; set; }
+    }
+
+    public class GeminiRequest
+    {
+        public List<GeminiContent> contents { get; set; }
+    }
+}
