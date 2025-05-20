d6eb50d52817c88ddac748a86de2fddc79530248
Pablofvb07 <pablovargas44@gmail.com>
2025-05-20 15:29:33 -0500

    Agregar archivos de proyecto.

---
+++ HerreraSierraVargas_ChatBot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HerreraSierraVargas_ChatBot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
